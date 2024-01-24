0e0508ff3d2403e8e863b70166eee2a4b6ea3f58
Anton Chernichkin <tonynba@yandex.ru>
2024-01-24 19:30:18 +0100

    Задал основные локаторы для сценария с заказом

---
+++ locators/base_page_locators.py
@@ -0,0 +1,4 @@
+from selenium.webdriver.common.by import By
+
+logo = [By.XPATH, '//img[@alt="Scooter"]'] #Логотип
+button_order_header = [By.XPATH, '//div[@class="Header_Nav__AGCXC"]/button[text()= "Заказать"]'] #Заказать из хэдера
--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -1,4 +1,3 @@
 from selenium.webdriver.common.by import By
 
-button_order_header = [By.XPATH, '//div[@class="Header_Nav__AGCXC"]/button[text()= "Заказать"]']
 button_order_center = [By.XPATH, '//div[@class="Home_FinishButton__1_cWm"]/button]']
